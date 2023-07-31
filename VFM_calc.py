b325131a9acb0f13689ed40b08e2469c01b22155
Yoshihiro Ashihara <yoshihiro.ashihara@gmail.com>
2023-07-31 17:45:16 +0900

    misc

--- VFM_calc.py
+++ VFM_calc.py
@@ -65,9 +65,9 @@ class PSC:
         risk_adj_const = risk_adj * PSC_const_rate
         risk_adj_ijikanri = risk_adj * PSC_ijikanri_rate
 
-        PSC_net_expense_const = (risk_adj_const + shisetsu_seibi_total + ribarai_kan + hojokin_kan) * rakusatsu_ritsu
-        PSC_net_expense_ijikanri = (risk_adj_ijikanri + ijikanri_unnei_total) * rakusatsu_ritsu
-        
+        PSC_net_expense_const_kk = (risk_adj_const + shisetsu_seibi_total + ribarai_kan + hojokin_kan) * rakusatsu_ritsu
+        PSC_net_expense_ijikanri_kk = (risk_adj_ijikanri + ijikanri_unnei_total) * rakusatsu_ritsu
+
         #NPV_PSC = PSC_net_expense / ((1 + discount_rate/100)**int(self.final_inputs['proj_years']))
     
 class VFM:
