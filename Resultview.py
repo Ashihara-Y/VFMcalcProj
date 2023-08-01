7dbcb1599c8c757ed878459451f2b5f106b0c522
Yoshihiro Ashihara <yoshihiro.ashihara@gmail.com>
2023-08-01 20:05:55 +0900

    added Card

--- Resultview.py
+++ Resultview.py
@@ -20,22 +20,33 @@ class Results(ft.UserControl):
 
     def build(self):
 
-        self.b = ft.ElevatedButton(text="結果表示", on_click=self.button_clicked)
-        self.b2 = ft.ElevatedButton(text="結果表示2", on_click=self.view_make)
-        return ft.Column([self.b, self.b2], scroll=ft.ScrollMode.ALWAYS)
+        #self.b = ft.ElevatedButton(text="結果表示", on_click=self.button_clicked)
+        #self.b2 = ft.ElevatedButton(text="結果表示2", on_click=self.view_make)
+        df_PV_cf = self.results['df_PV_cf']
+        self.fig = px.bar(df_PV_cf, x=df_PV_cf.index, y=['PSC_present_value', 'LCC_present_value'], barmode='group')
+        self.graph = PlotlyChart(self.fig, expand=True)
+        return ft.Card(
+            content=ft.Container(
+                content=ft.Column(
+                    controls=[
+                        self.graph,
+                    ],
+                    alignment=ft.MainAxisAlignment.SPACE_AROUND,
+                ),
+                width=800,
+                padding=16,
+            )
+        )        #return ft.Column([self.b, self.b2], scroll=ft.ScrollMode.ALWAYS)
     
     
 #class VFM:
-    def button_clicked(self,e):
-        #to Plotly_chart
-        df_PV_cf = self.results['df_PV_cf']
-        fig = px.bar(df_PV_cf, x=df_PV_cf.index, y=['PSC_present_value', 'LCC_present_value'], barmode='group')
-        #ft.Page.page.add(PlotlyChart(self.fig, expand=True)) 
-        return fig
+#    def button_clicked(self,e):
+#        #to Plotly_chart
+        
     
-    def view_make(self, e, page: ft.Page):
-        self.self.fig = self.button_clicked(self)
-        #page.add(self.simpledt_dt)
-        page.add(PlotlyChart(self.fig, expand=True))
-        page.update()
+#    def view_make(self, e, page: ft.Page):
+#        self.self.fig = self.button_clicked(self)
+#        #page.add(self.simpledt_dt)
+#        page.add(PlotlyChart(self.fig, expand=True))
+#        page.update()
         
