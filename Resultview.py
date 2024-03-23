540241b1e1dbac91e00761f09874c2ed2c24f235
Yoshihiro Ashihara <yoshihiro.ashihara@gmail.com>
2024-03-23 16:39:50 +0900

    misc

--- Resultview.py
+++ Resultview.py
@@ -8,6 +8,7 @@ from flet.plotly_chart import PlotlyChart
 import tinydb
 from tinydb import TinyDB, Query
 import glob
+import openpyxl
 
 
 # savedir = pathlib.Path(mkdtemp(prefix=None, suffix=None, dir='.')) # 一時ディレクトリを作成
@@ -108,69 +109,7 @@ class Results(ft.UserControl):
             )
         )
     def export_to_excel():
-        
-class Results_detail(ft.UserControl):
-    def __init__(self):
-        super().__init__()
-        self.title = "結果  詳細"
-        self.width = 1000
-        self.height = 800
-        self.resizable = True
+        wb = openpyxl.load_workbook('VFM_result_sheet.xlsx')
+        ws = wb.active
+        ws.append(['PSC', 'LCC', 'VFM', 'VFM_percent'])
 
-        con = TinyDB('res_detail.json')
-        self.res_detail = con.all()
-        #self.final_inputs = joblib.load('final_inputs.joblib')　除書でｎ以下に渡して処理
-        #self.res_PSC_LCC = joblib.load('res_PSC_LCC.joblib')　ｄ以下ｄ使っていない
-        #self.results = joblib.load("results.joblib") DFでｎ以下に渡して処理
-
-    def build(self):
-        df_PV_cf = self.results["df_PV_cf"].round(3)
-        self.fig = px.bar(
-            df_PV_cf,
-            x=df_PV_cf.index,
-            y=["PSC_present_value", "LCC_present_value"],
-            barmode="group",
-        )
-        self.graph = PlotlyChart(self.fig, expand=True)
-
-        # to ft.datatable
-        simpledt_df = DataFrame(df_PV_cf.transpose())
-        simpledt_dt = simpledt_df.datatable
-        self.table = simpledt_dt
-
-        lv = ft.ListView(
-            expand=1, spacing=10, padding=20, auto_scroll=True, horizontal=True
-        )
-        lv.controls.append(self.table)
-
-        PSC = round(float(self.results["PSC"]), 3)
-        LCC = round(float(self.results["LCC"]), 3)
-        VFM = round(float(self.results["VFM"]), 3)
-        VFM_percent = self.results["VFM_percent"]
-        self.tx_PSC = ft.Text("PSC(百万円): ", style=ft.TextThemeStyle.HEADLINE_SMALL)
-        self.v_PSC = ft.Text(PSC, style=ft.TextThemeStyle.HEADLINE_SMALL)
-        self.tx_LCC = ft.Text("LCC(百万円): ", style=ft.TextThemeStyle.HEADLINE_SMALL)
-        self.v_LCC = ft.Text(LCC, style=ft.TextThemeStyle.HEADLINE_SMALL)
-        self.tx_VFM = ft.Text("VFM(百万円): ", style=ft.TextThemeStyle.HEADLINE_SMALL)
-        self.v_VFM = ft.Text(VFM, style=ft.TextThemeStyle.HEADLINE_SMALL)
-        self.tx_VFM_percent = ft.Text(
-            "VFM(%): ", style=ft.TextThemeStyle.HEADLINE_SMALL
-        )
-        self.v_VFM_percent = ft.Text(
-            VFM_percent, style=ft.TextThemeStyle.HEADLINE_SMALL
-        )
-
-        return ft.Card(
-            content=ft.Container(
-                content=ft.Column(
-                    controls=[
-                        self.graph,
-                        lv,
-                    ],
-                    alignment=ft.MainAxisAlignment.SPACE_AROUND,
-                ),
-                # content=lv,
-                width=1000,
-                padding=16,
-            )
-        )
