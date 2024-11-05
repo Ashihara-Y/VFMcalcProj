29100e2fd75881f4bdaa3c79aa0a049d7070a20d
Yoshihiro ASHIHARA <yoshihiro.ashihara@gmail.com>
2024-11-06 08:14:08 +0900

    making zero-pls

--- make_empty_pls.py
+++ make_empty_pls.py
@@ -3,15 +3,12 @@ import os
 import pandas as pd
 import tinydb
 from tinydb import TinyDB, Query
-import pyxirr
 
 from dataclasses import asdict, dataclass
-import datetime
 from decimal import *
 from pydantic import BaseModel
 import pandera as pa
 from pandera.typing import Series, DataFrame
-import duckdb
 import make_inputs_df, make_pl_waku
 
 inputs_pdt, inputs_supl_pdt = make_inputs_df.io()
@@ -22,12 +19,12 @@ target_years = inputs_supl_pdt.target_years.iloc[0]
 first_end_fy = inputs_supl_pdt.first_end_fy.iloc[0]
 
 waku = make_pl_waku.make_waku_df(first_end_fy, target_years, cols_waku)
-print(waku.info())
+#print(waku.info())
 
-cols_PSC_income = ['periods','hojokin','kouhukin','kisai_gaku','riyou_ryoukin']
-added_cols = make_pl_waku.add_cols_df(target_years, waku, cols_PSC_income)
-print(added_cols.info())
 
-empty_pl_PSC_income = make_pl_waku.join_cols2waku(added_cols, waku)
+def make_empty_pl(cols):
+    added_cols = make_pl_waku.add_cols_df(target_years, waku, cols)
+    empty_pl = make_pl_waku.join_cols2waku(added_cols, waku)
+    return empty_pl
 
-print(empty_pl_PSC_income.info())
+
