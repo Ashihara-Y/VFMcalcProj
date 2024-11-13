a74e98513d50f2f1b8ca347e82ca539ee0d15916
Yoshihiro Ashihara <yoshihiro.ashihara@gmail.com>
2024-11-13 20:20:23 +0900

    misc

--- check_SPC_cashForPPayment.py
+++ check_SPC_cashForPPayment.py
@@ -1,10 +1,8 @@
 import pandas as pd
-import tinydb
 from tinydb import TinyDB, Query
 from pyxirr import xirr
 import duckdb
 from dataclasses import asdict, dataclass
-import datetime
 from decimal import *
 from pydantic import BaseModel
 import pandera as pa
@@ -12,7 +10,6 @@ from pandera.typing import Series, DataFrame
 import openpyxl
 import make_inputs_df, make_pl_waku, make_empty_pls, make_3pls_withZero
 
-zero_pl_PSC_income, zero_pl_PSC_payments, zero_pl_LCC_income, zero_pl_LCC_payments, zero_pl_SPC_income, zero_pl_SPC_payments = make_3pls_withZero.output()
 inputs_pdt, inputs_supl_pdt = make_inputs_df.io()
 
 conn = duckdb.connect('VFM.duckdb')
@@ -32,27 +29,27 @@ SPC_df['Cash_for_P_payment'] = SPC_df['income_total'] - (SPC_df['kariire_ganpon_
 
 SPC_df['P_payment_check'] = SPC_df['Cash_for_P_payment'] >= 0
 SPC_PPayment_check_ls = SPC_df.loc[inputs_pdt.const_years+1:inputs_pdt.proj_years, 'P_payment_check'].tolist()
-print(SPC_df)
+#print(SPC_df)
 
 year = SPC_df['year'].tolist()
 value = SPC_df['net_income'].tolist()
 
 PIRR = xirr(year, value)
 PIRR_percent = PIRR * 100
-print(PIRR, PIRR_percent)
+#print(PIRR, PIRR_percent)
 
-print(SPC_df[['income_total','payments_total_full','net_income_full']])
+#print(SPC_df[['income_total','payments_total_full','net_income_full']])
 
 net_total_income_sum = float(SPC_df['net_income_full'].sum())
 SPC_shihon = float(inputs_pdt.SPC_shihon)
 year_factor = float(1 / inputs_pdt.proj_years)
-print(net_total_income_sum, SPC_shihon, year_factor)
+#print(net_total_income_sum, SPC_shihon, year_factor)
 
 
 value_EIRR = SPC_df['net_income_full'].to_list()
 EIRR = xirr(year, value_EIRR)
 EIRR_percent = EIRR * 100
-print(EIRR, EIRR_percent)
+#print(EIRR, EIRR_percent)
 
 PIRR_df = pd.DataFrame({'PIRR': [PIRR], 'PIRR_percent': [PIRR_percent]})
 c.execute('CREATE OR REPLACE TABLE PIRR_table AS SELECT * from PIRR_df')
