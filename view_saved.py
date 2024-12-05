8bdad1b6459086b99e16d1d172a49d5e2cf1becb
Yoshihiro Ashihara <yoshihiro.ashihara@gmail.com>
2024-12-05 10:55:09 +0900

    modified view_saved

--- view_saved.py
+++ view_saved.py
@@ -5,13 +5,12 @@ import pandas as pd
 import flet as ft
 import simpledt
 from simpledt import DataFrame
-import plotly.express as px
+#import plotly.express as px
 from flet.plotly_chart import PlotlyChart
-import glob
+#import glob
 import Resultview
-import tinydb
-from tinydb import TinyDB, Query
-
+import duckdb
+from tinydb import TinyDB
 
 
 class View_saved(ft.Column):
@@ -22,70 +21,63 @@ class View_saved(ft.Column):
         self.height = 1800
         self.resizable = True
 
-        # 以下を、DBからの読み込みに切り替えるとして、どのテーブルを読み込むか？
-        # ここでは、「全結果のリスト」を作成している。
-        save_res_01_list = glob.glob("res_01_db_*.json")
-        save_res_01_list.sort(reverse=True)
+        conn = duckdb.connect('VFM.duckdb')
+        c = conn.cursor()
         
-        self.res_summ_list = []
-
-        # 詳細結果は、ファイル読み込みではなく、DB各テーブルの内容そのものに切り替える必要。
-        # なので、Datetimeで必要なテーブルから各結果を抽出して、要約用のDFを作成する必要。
-        # それとも、「aave_results.py」の最後で、要約用DFを作成して、DuckDBに入れておいた
-        # 方が早いのでは？ 少なくともDatetime、UserID、CalcIDを挿入する段階なら、作成
-        # できるはず。
-        for file in save_res_01_list:        
-            con = TinyDB(file)
-            res_detail = con.all()[0]
-            con.close()
-            res_detail_df = pd.DataFrame(data=res_detail, index=[0])
-            
-            # DBの各テーブルからの読み込み結果から、以下のカラムのDFを作成する。
-            res_summary = res_detail_df.reindex(
-                columns=[
-                    "datetime",
-                    "VFM_percent",
-                    "PSC_pv",
-                    "LCC_pv",
-                    "P-IRR",
-                    "mgmt_type",
-                    "proj_ctgry",
-                    "proj_type",
-                    "proj_years",
-                    "const_years",
-                    "discount_rate",
-                ]
-            )
-            res_summary = res_summary.rename(
-                columns={
-                    "datetime": "作成日時",
-                    "VFM_percent": "VFM(%)",
-                    "PSC_pv": "PSC現在価値総額",
-                    "LCC_pv": "LCC現在価値総額",
-                    "P-IRR": "プロジェクトの内部収益率",
-                    "mgmt_type": "施設管理者種別",
-                    "proj_ctgry": "事業類型",
-                    "proj_type": "事業方式",
-                    "proj_years": "事業期間",
-                    "const_years": "施設整備期間",
-                    "discount_rate": "割引率",
-                },
+        res_summ_df = c.sql('select * from result_summary_df_results_table').df()
+        grd_df_exp = res_summ_df.groupby('datetime').apply(lambda x: x.head(1), include_groups=False)
+        grd_df_exp_ri = grd_df_exp.reset_index().drop('level_1', axis=1)
+        grd_df_exp_ri2 = grd_df_exp_ri[[
+            'datetime',
+            'VFM_percent',
+            'PSC_present_value',
+            'LCC_present_value',
+            'PIRR',
+            'kariire_kinri',
+            'discount_rate',
+            'SPC_payment_cash',
+            'mgmt_type',
+            'proj_ctgry',
+            'proj_type',
+            'proj_years',
+            'const_years'
+            ]]
+        
+        res_summ_list = grd_df_exp_ri2.rename(
+            columns={
+            "datetime": "算定日時",
+            "VFM_percent": "VFM(%)",
+            "PSC_present_value": "PSC現在価値総額",
+            "LCC_present_value": "LCC現在価値総額",
+            "PIRR": "プロジェクトの内部収益率(%)",
+            "kariire_kinri": "借入コスト(%)",
+            "discount_rate": "割引率(%)",
+            "SPC_payment_cash":"SPCのキャッシュ水準",
+            "mgmt_type": "施設管理者種別",
+            "proj_ctgry": "事業類型",
+            "proj_type": "事業方式",
+            "proj_years": "事業期間",
+            "const_years": "施設整備期間",
+            },
                 index={0: "項目"},
             )
-            self.res_summ_list.append(res_summary)
 
+    # 以下のメソッドは、選択された日時を、次の画面に渡すためのメソッド。
+    # このメソッドは、ListViewのセルを選択したときに呼び出される。
+    # このメソッドは、選択された日時を、selected_res.jsonに書き込む。
+    # このメソッドは、選択された日時を、次の画面に渡す。
+    # session storageni書き込む形に変更する。
     def send_mess(self, e):
         #ft.Page.pubsub.send_all(Message)
         if os.path.exists("selected_res.json"):
             os.remove("selected_res.json")
         con = TinyDB('selected_res.json')
         con.truncate()
-        #dtime = e.control.cells[0].content.value
         dtime = e.control.data
-        #print(dtime)
         dtime_dic = {'selected_datetime': str(dtime)}
         con.insert(dtime_dic)
         con.close()
+        self.page.session.set(dtime_dic)
         self.page.go("/results_detail")
 
     def build(self):
@@ -99,24 +91,24 @@ class View_saved(ft.Column):
             horizontal=False,
         )
 
-        for df in self.res_summ_list:
-            #self.dtime = str(df['作成日時'].iloc[0])
-            df = DataFrame(df)
+        for row in self.res_summ_list.intertuples():
+            row = pd.DataFrame(row).drop(0, axis=0)
+            df = DataFrame(row)
             dr = df.datarows
+            dtime = dr[0].cells[0].content.value
+            dr[0].data = dtime
             for i in dr:
-                dtime = i.cells[0].content.value
-                i.data = dtime
-                i.color=ft.colors.AMBER_50
+                i.data = dtime                
+                i.color=ft.Colors.AMBER_50
                 i.selected=False
-                #message = {'selected_datetime': dtime}
                 i.on_long_press=self.send_mess
                 i.on_selectd_changed=self.send_mess
+
             table = df.datatable
             # ここで、DTに修飾を追加する。チェックボックス、色、テキストスタイル
             table.width=1500
-            #table.bg_color=ft.colors.AMBER_50
             table.show_checkbox_column=True
-            table.checkbox_column_width=100
+            table.checkbox_column_width=15
             table.checkbox_horizontal_margin=10
             table.on_select_all=True
             
