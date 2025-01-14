037b9aff2a8d4959099b3f16afd19851b2807d59
Yoshihiro ASHIHARA <yoshihiro.ashihara@gmail.com>
2025-01-14 23:12:53 +0900

    added item_name to results_list

--- view_saved.py
+++ view_saved.py
@@ -47,22 +47,22 @@ class View_saved(ft.Column):
             ]]
         
         self.res_summ_list = grd_df_exp_ri2.rename(
-            columns={
-            "datetime": "算定日時",
-            "VFM_percent": "VFM(%)",
-            "PSC_present_value": "PSC現在価値総額",
-            "LCC_present_value": "LCC現在価値総額",
-            "PIRR": "プロジェクトの内部収益率(%)",
-            "kariire_kinri": "借入コスト(%)",
-            "discount_rate": "割引率(%)",
-            "SPC_payment_cash":"SPCのキャッシュ水準",
-            "mgmt_type": "施設管理者種別",
-            "proj_ctgry": "事業類型",
-            "proj_type": "事業方式",
-            "proj_years": "事業期間",
-            "const_years": "施設整備期間",
-            },
-                index={0: "項目"},
+            #columns={
+            #"datetime": "算定日時",
+            #"VFM_percent": "VFM(%)",
+            #"PSC_present_value": "PSC現在価値総額",
+            #"LCC_present_value": "LCC現在価値総額",
+            #"PIRR": "プロジェクトの内部収益率(%)",
+            #"kariire_kinri": "借入コスト(%)",
+            #"discount_rate": "割引率(%)",
+            #"SPC_payment_cash":"SPCのキャッシュ水準",
+            #"mgmt_type": "施設管理者種別",
+            #"proj_ctgry": "事業類型",
+            #"proj_type": "事業方式",
+            #"proj_years": "事業期間",
+            #"const_years": "施設整備期間",
+            #},
+                index={0: 0},
             )
 
     # 以下のメソッドは、選択された日時を、次の画面に渡すためのメソッド。
@@ -94,12 +94,30 @@ class View_saved(ft.Column):
         )
 
         for row in self.res_summ_list.itertuples():
-            row = pd.DataFrame(row).drop(0, axis=0)
+            row_dic = row._asdict()
+            row = pd.DataFrame(row_dic, index=[0])
             dtime = row['datetime'].iloc[0]
-            row = row.tranpose().reset_index()
+            row = row.rename(
+                columns={
+                    "datetime": "算定日時",
+                    "VFM_percent": "VFM(%)",
+                    "PSC_present_value": "PSC現在価値総額",
+                    "LCC_present_value": "LCC現在価値総額",
+                    "PIRR": "プロジェクトの内部収益率(%)",
+                    "kariire_kinri": "借入コスト(%)",
+                    "discount_rate": "割引率(%)",
+                    "SPC_payment_cash":"SPCのキャッシュ水準",
+                    "mgmt_type": "施設管理者種別",
+                    "proj_ctgry": "事業類型",
+                    "proj_type": "事業方式",
+                    "proj_years": "事業期間",
+                    "const_years": "施設整備期間",
+                },
+            )
+            row = row.T.reset_index()
+            row = row.rename(columns={"index":"項目名", 0:"値"}).drop(0, axis=0)
             df = DataFrame(row)
             dr = df.datarows
-            #dtime = dr[0].cells[0].content.value
             for j in range(len(dr)):
                 dr[j].data = dtime
                 for i in dr[j]:
