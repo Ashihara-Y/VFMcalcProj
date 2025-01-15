2b31217bac74d6eaf5cda1fca83293f1c1a5450e
Yoshihiro Ashihara <yoshihiro.ashihara@gmail.com>
2025-01-15 15:02:52 +0900

    translate item_names to Japanese

--- Resultview2.py
+++ Resultview2.py
@@ -64,13 +64,145 @@ class Results(ft.Stack):
         PIRR_res_df = self.selected_res_list[8]
         res_summ_df = self.selected_res_list[9]
 
+        PSC_res_df = PSC_res_df.drop(['datetime', 'user_id', 'calc_id'], axis=1)
+        PSC_pv_df =  PSC_pv_df.drop(['datetime', 'user_id', 'calc_id'], axis=1)
+        LCC_res_df = LCC_res_df.drop(['datetime', 'user_id', 'calc_id'], axis=1)
+        LCC_pv_df = LCC_pv_df.drop(['datetime', 'user_id', 'calc_id'], axis=1)
+        SPC_res_df = SPC_res_df.drop(['datetime', 'user_id', 'calc_id'], axis=1)
+        SPC_check_df = SPC_check_df.drop(['datetime', 'user_id', 'calc_id'], axis=1)
+        Risk_res_df = Risk_res_df.drop(['datetime', 'user_id', 'calc_id'], axis=1)
+        VFM_res_df = VFM_res_df.drop(['datetime', 'user_id', 'calc_id'], axis=1)
+        PIRR_res_df = PIRR_res_df.drop(['datetime', 'user_id', 'calc_id'], axis=1)
+        res_summ_df = res_summ_df.drop(['datetime', 'user_id', 'calc_id'], axis=1)
+
         PSC_res_df = PSC_res_df.rename(
             columns={
-                "datetime": "算定日時",
+                'periods':'経過年度', 
+                'year':'スケジュール', 
+                'hojokin':'補助金', 
+                'kouhukin':'交付金', 
+                'kisai_gaku':'起債発行額', 
+                'riyou_ryoukin':'利用料金収入',
+                'income_total':'収入計', 
+                'shisetsu_seibihi':'施設整備費用', 
+                'ijikanri_unneihi':'維持管理運営費用',
+                'monitoring_costs':'モニタリング等費用', 
+                'chisai_zansai':'(起債残債)', 
+                'kisai_shoukan_gaku':'起債償還額',
+                'kisai_shoukansumi_gaku':'(起債償還済額)', 
+                'kisai_risoku_gaku':'起債利息', 
+                'payments_total':'支出計',
+                'net_payments':'収支（キャッシュ・フロー）', 
+            }
+        )
+        PSC_pv_df = PSC_pv_df.rename(
+            columns={
+                'net_payments':'収支（キャッシュ・フロー）', 
+                'discount_factor':'割引係数', 
+                'present_value':'収支（キャッシュ・フロー）現在価値', 
+            }
+        )
+        LCC_res_df = LCC_res_df.rename(
+            columns={
+                'periods':'経過年度', 
+                'year':'スケジュール', 
+                'hojokin':'補助金', 
+                'kouhukin':'交付金', 
+                'kisai_gaku':'起債発行額', 
+                'zeishu':'税収',
+                'income_total':'収入計', 
+                'shisetsu_seibihi_ikkatsu':'施設整備サービス対価(一括払)',
+                'shisetsu_seibihi_kappugoukei':'(施設整備サービス対価(割賦計）)', 
+                'shisetsu_seibihi_kappuganpon':'施設整備サービス対価(割賦元本)',
+                'shisetsu_seibihi_kappukinri':'施設整備サービス対価(割賦金利)', 
+                'ijikanri_unneihi':'維持管理費サービス対価', 
+                'monitoring_costs':'モニタリング等費用',
+                'SPC_keihi':'SPC費用', 
+                'chisai_zansai':'(起債残債)', 
+                'kisai_shoukan_gaku':'起債償還額',
+                'kisai_shoukansumi_gaku':'(起債償還済額)', 
+                'kisai_risoku_gaku':'起債利息', 
+                'payments_total':'支出計',
+                'net_payments':'収支', 
+            }
+        )
+        LCC_pv_df = LCC_pv_df.rename(
+            columns={
+                'net_payments':'収支(キャッシュ・フロー)', 
+                'discount_factor':'割引係数', 
+                'present_value':'収支(キャッシュ・フロー)現在価値',
+            }
+        )
+        SPC_res_df = SPC_res_df.rename(
+            columns={
+                'periods':'経過年度', 
+                'year':'スケジュール', 
+                'shisetsu_seibihi_taika_ikkatsu':'施設整備サービス対価(一括払)',
+                'shisetsu_seibihi_taika_kappuganpon':'施設整備サービス対価(割賦元本)',
+                'shisetsu_seibihi_taika_kappukinri':'施設整備サービス対価(割賦金利)', 
+                'ijikanri_unneihi_taika':'維持管理費サービス対価',
+                'SPC_hiyou_taika':'SPC費用サービス対価', 
+                'riyou_ryoukin':'利用料金収入', 
+                'income_total':'収入計', 
+                'shisetsu_seibihi':'施設整備費用',
+                'ijikanri_unneihi':'維持管理費', 
+                'kariire_ganpon_hensai':'(借入元本返済)', 
+                'shiharai_risoku':'支払利息',
+                'SPC_keihi':'SPC経費', 
+                'SPC_setsuritsuhi':'SPC当初費用（設立費用、予備費）', 
+                'houjinzei_etc':'法人税・公租公課', 
+                'payments_total_full':'支出計（元本返済込）',
+                'payments_total':'支出計', 
+                'net_income':'収支(キャッシュ・フロー)', 
+            }
+        )
+        SPC_check_df = SPC_check_df.rename(
+            columns={
+                'year':'スケジュール', 
+                'income_total':'収入計', 
+                'kariire_ganpon_hensai':'借入元本返済', 
+                'payments_total':'支出計',
+                'payments_total_full':'支出計(元本返済込)', 
+                'net_income':'収支(キャッシュ・フロー)', 
+                'net_income_full':'収支(元本返済込)',
+                'Cash_for_P_payment':'元本返済充当可能額', 
+                'P_payment_check':'元本返済可否', 
+            }
+        )
+        Risk_res_df = Risk_res_df.rename(
+            columns={
+                'risk_adjust_gaku':'リスク調整額', 
+            }
+        )
+        VFM_res_df = Risk_res_df.rename(
+            columns={
+                'VFM':'VFM(金額)', 
+                'VFM_percent':'VFM(％)', 
+            }
+        )
+        PIRR_res_df = PIRR_res_df.rename(
+            columns={
+                'PIRR':'プロジェクト内部収益率', 
+                'PIRR_percent':'プロジェクト内部収益率(％)', 
                 
             }
         )
-
+        res_summ_df = res_summ_df.rename(
+            columns={
+                'VFM_percent':'VFM(％)', 
+                'PSC_present_value':'PSCでの公共キャッシュ・フロー現在価値', 
+                'LCC_present_value':'PFI-LCCでの公共キャッシュ・フロー現在価値', 
+                'PIRR':'プロジェクト内部収益率(％)',
+                'SPC_payment_cash':'SPCの元本返済可否', 
+                'mgmt_type':'発注者区分', 
+                'proj_ctgry':'事業形態', 
+                'proj_type':'事業方式',
+                'const_years':'施設整備期間', 
+                'proj_years':'事業期間', 
+                'discount_rate':'割引率(％)', 
+                'kariire_kinri':'借入コスト(％)',
+            }
+        )
         # 総括表をここに追加する。PSC,LCC、SPCの表も作成して、別個定義しておく。
         simpledt_PSC_df = DataFrame(PSC_res_df)
         simpledt_PSC_dt = simpledt_PSC_df.datatable
