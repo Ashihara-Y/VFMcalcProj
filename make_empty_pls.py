93887e6af4072d3ff7a267c767ee95fd551a1c6d
Yoshihiro Ashihara <yoshihiro.ashihara@gmail.com>
2025-01-19 21:27:04 +0900

    misc

--- make_empty_pls.py
+++ make_empty_pls.py
@@ -9,14 +9,17 @@ from decimal import *
 from pydantic import BaseModel
 import make_inputs_df, make_pl_waku
 
+db = TinyDB("fi_db.json")
+final_inputs = db.all()[0]
+
 
 def make_empty_pl(cols):
-    inputs_pdt = make_inputs_df.main()
+    #inputs_pdt = make_inputs_df.main()
 
     cols_waku = ['year','periods']
 
-    target_years = inputs_pdt.target_years
-    first_end_fy = inputs_pdt.first_end_fy
+    target_years = final_inputs.target_years
+    first_end_fy = final_inputs.first_end_fy
 
     waku = make_pl_waku.make_waku_df(first_end_fy, target_years, cols_waku)
 
