4cd645cd02fc4a349b1e076bbb99701cae7e761d
Yoshihiro ASHIHARA <yoshihiro.ashihara@gmail.com>
2025-01-21 22:43:47 +0900

    divide shuushi-table to income & payments

--- Resultview2.py
+++ Resultview2.py
@@ -64,18 +64,25 @@ class Results(ft.Stack):
         PIRR_res_df = self.selected_res_list[8]
         res_summ_df = self.selected_res_list[9]
 
-        PSC_res_df = PSC_res_df.drop(['datetime', 'user_id', 'calc_id'], axis=1)
+        PSC_res_df = PSC_res_df.drop(['chisai_zansai', 'kisai_shoukansumi_gaku', 'datetime', 'user_id', 'calc_id'], axis=1)
         PSC_pv_df =  PSC_pv_df.drop(['datetime', 'user_id', 'calc_id'], axis=1)
-        LCC_res_df = LCC_res_df.drop(['datetime', 'user_id', 'calc_id'], axis=1)
+        LCC_res_df = LCC_res_df.drop(['shisetsu_seibihi_kappugoukei', 'chisai_zansai', 'kisai_shoukansumi_gaku', 'datetime', 'user_id', 'calc_id'], axis=1)
         LCC_pv_df = LCC_pv_df.drop(['datetime', 'user_id', 'calc_id'], axis=1)
-        SPC_res_df = SPC_res_df.drop(['datetime', 'user_id', 'calc_id'], axis=1)
-        SPC_check_df = SPC_check_df.drop(['datetime', 'user_id', 'calc_id'], axis=1)
+        SPC_res_df = SPC_res_df.drop(['payments_total_full', 'datetime', 'user_id', 'calc_id'], axis=1)
+        SPC_check_df = SPC_check_df.drop(['payments_total_full', 'net_income_full',  'datetime', 'user_id', 'calc_id'], axis=1)
         Risk_res_df = Risk_res_df.drop(['datetime', 'user_id', 'calc_id'], axis=1)
         VFM_res_df = VFM_res_df.drop(['datetime', 'user_id', 'calc_id'], axis=1)
         PIRR_res_df = PIRR_res_df.drop(['datetime', 'user_id', 'calc_id'], axis=1)
         res_summ_df = res_summ_df.drop(['datetime', 'user_id', 'calc_id'], axis=1)
 
-        PSC_res_df = PSC_res_df.rename(
+        PSC_res_income_df = PSC_res_df[['periods','year','hojokin', 'kouhukin', 'kisai_gaku', 'riyou_ryoukin', 'income_total']]
+        PSC_res_payments_df = PSC_res_df[['periods','year','shisetsu_seibihi', 'ijikanri_unneihi', 'monitoring_costs', 'kisai_shoukan_gaku', 'kisai_risoku_gaku', 'payments_total', 'net_payments']]
+        LCC_res_income_df = LCC_res_df[['periods','year','hojokin', 'kouhukin', 'kisai_gaku', 'zeishu', 'income_total']]
+        LCC_res_payments_df = LCC_res_df[['periods','year','shisetsu_seibihi_ikkatsu', 'shisetsu_seibihi_kappuganpon', 'shisetsu_seibihi_kappukinri', 'ijikanri_unneihi', 'monitoring_costs', 'SPC_keihi', 'kisai_shoukan_gaku', 'kisai_risoku_gaku', 'payments_total', 'net_payments']]
+        SPC_res_income_df = SPC_res_df[['periods','year','shisetsu_seibihi_taika_ikkatsu', 'shisetsu_seibihi_taika_kappuganpon', 'shisetsu_seibihi_taika_kappukinri', 'ijikanri_unneihi_taika', 'SPC_hiyou_taika', 'riyou_ryoukin', 'income_total']]
+        SPC_payments_df = SPC_res_df[['periods','year','shisetsu_seibihi', 'ijikanri_unneihi', 'shiharai_risoku', 'SPC_keihi', 'SPC_setsuritsuhi', 'houjinzei_etc', 'payments_total', 'net_payments']]
+
+        PSC_res_income_df = PSC_res_income_df.rename(
             columns={
                 'periods':'経過年度', 
                 'year':'スケジュール', 
@@ -84,12 +91,16 @@ class Results(ft.Stack):
                 'kisai_gaku':'起債発行額', 
                 'riyou_ryoukin':'利用料金収入',
                 'income_total':'収入計', 
+            }
+        )
+        PSC_res_payments_df = PSC_res_payments_df.rename(
+            columns={
+                'periods':'経過年度', 
+                'year':'スケジュール', 
                 'shisetsu_seibihi':'施設整備費用', 
                 'ijikanri_unneihi':'維持管理運営費用',
                 'monitoring_costs':'モニタリング等費用', 
-                'chisai_zansai':'(起債残債)', 
                 'kisai_shoukan_gaku':'起債償還額',
-                'kisai_shoukansumi_gaku':'(起債償還済額)', 
                 'kisai_risoku_gaku':'起債利息', 
                 'payments_total':'支出計',
                 'net_payments':'収支（キャッシュ・フロー）', 
@@ -102,7 +113,7 @@ class Results(ft.Stack):
                 'present_value':'収支（キャッシュ・フロー）現在価値', 
             }
         )
-        LCC_res_df = LCC_res_df.rename(
+        LCC_res_income_df = LCC_res_income_df.rename(
             columns={
                 'periods':'経過年度', 
                 'year':'スケジュール', 
@@ -111,16 +122,19 @@ class Results(ft.Stack):
                 'kisai_gaku':'起債発行額', 
                 'zeishu':'税収',
                 'income_total':'収入計', 
+            }
+        )
+        LCC_res_payments_df = LCC_res_payments_df.rename(
+            columns={
+                'periods':'経過年度', 
+                'year':'スケジュール', 
                 'shisetsu_seibihi_ikkatsu':'施設整備サービス対価(一括払)',
-                'shisetsu_seibihi_kappugoukei':'(施設整備サービス対価(割賦計）)', 
                 'shisetsu_seibihi_kappuganpon':'施設整備サービス対価(割賦元本)',
                 'shisetsu_seibihi_kappukinri':'施設整備サービス対価(割賦金利)', 
                 'ijikanri_unneihi':'維持管理費サービス対価', 
                 'monitoring_costs':'モニタリング等費用',
                 'SPC_keihi':'SPC費用', 
-                'chisai_zansai':'(起債残債)', 
                 'kisai_shoukan_gaku':'起債償還額',
-                'kisai_shoukansumi_gaku':'(起債償還済額)', 
                 'kisai_risoku_gaku':'起債利息', 
                 'payments_total':'支出計',
                 'net_payments':'収支', 
@@ -133,7 +147,7 @@ class Results(ft.Stack):
                 'present_value':'収支(キャッシュ・フロー)現在価値',
             }
         )
-        SPC_res_df = SPC_res_df.rename(
+        SPC_res_income_df = SPC_res_income_df.rename(
             columns={
                 'periods':'経過年度', 
                 'year':'スケジュール', 
@@ -144,6 +158,12 @@ class Results(ft.Stack):
                 'SPC_hiyou_taika':'SPC費用サービス対価', 
                 'riyou_ryoukin':'利用料金収入', 
                 'income_total':'収入計', 
+            }
+        )
+        SPC_res_payments_df = SPC_res_payments_df.rename(
+            columns={
+                'periods':'経過年度', 
+                'year':'スケジュール', 
                 'shisetsu_seibihi':'施設整備費用',
                 'ijikanri_unneihi':'維持管理費', 
                 'kariire_ganpon_hensai':'(借入元本返済)', 
@@ -151,7 +171,6 @@ class Results(ft.Stack):
                 'SPC_keihi':'SPC経費', 
                 'SPC_setsuritsuhi':'SPC当初費用（設立費用、予備費）', 
                 'houjinzei_etc':'法人税・公租公課', 
-                'payments_total_full':'支出計（元本返済込）',
                 'payments_total':'支出計', 
                 'net_income':'収支(キャッシュ・フロー)', 
             }
@@ -162,9 +181,7 @@ class Results(ft.Stack):
                 'income_total':'収入計', 
                 'kariire_ganpon_hensai':'借入元本返済', 
                 'payments_total':'支出計',
-                'payments_total_full':'支出計(元本返済込)', 
                 'net_income':'収支(キャッシュ・フロー)', 
-                'net_income_full':'収支(元本返済込)',
                 'Cash_for_P_payment':'元本返済充当可能額', 
                 'P_payment_check':'元本返済可否', 
             }
@@ -174,7 +191,7 @@ class Results(ft.Stack):
                 'risk_adjust_gaku':'リスク調整額', 
             }
         )
-        VFM_res_df = Risk_res_df.rename(
+        VFM_res_df = VFM_res_df.rename(
             columns={
                 'VFM':'VFM(金額)', 
                 'VFM_percent':'VFM(％)', 
@@ -204,23 +221,35 @@ class Results(ft.Stack):
             }
         )
         # 総括表をここに追加する。PSC,LCC、SPCの表も作成して、別個定義しておく。
-        simpledt_PSC_df = DataFrame(PSC_res_df)
-        simpledt_PSC_dt = simpledt_PSC_df.datatable
-        self.table_PSC = simpledt_PSC_dt
+        simpledt_PSC_income_df = DataFrame(PSC_res_income_df)
+        simpledt_PSC_income_dt = simpledt_PSC_income_df.datatable
+        self.table_PSC_income = simpledt_PSC_income_dt
+        simpledt_PSC_payments_df = DataFrame(PSC_res_payments_df)
+        simpledt_PSC_payments_dt = simpledt_PSC_payments_df.datatable
+        self.table_PSC_payments = simpledt_PSC_payments_dt
+
         simpledt_PSC_pv_df = DataFrame(PSC_pv_df)
         simpledt_PSC_pv_dt = simpledt_PSC_pv_df.datatable
         self.table_PSC_pv = simpledt_PSC_pv_dt
 
-        simpledt_LCC_df = DataFrame(LCC_res_df)
-        simpledt_LCC_dt = simpledt_LCC_df.datatable
-        self.table_LCC = simpledt_LCC_dt
+        simpledt_LCC_income_df = DataFrame(LCC_res_income_df)
+        simpledt_LCC_income_dt = simpledt_LCC_income_df.datatable
+        self.table_LCC_income = simpledt_LCC_income_dt
+        simpledt_LCC_payments_df = DataFrame(LCC_res_payments_df)
+        simpledt_LCC_payments_dt = simpledt_LCC_payments_df.datatable
+        self.table_LCC_payments = simpledt_LCC_payments_dt
+
         simpledt_LCC_pv_df = DataFrame(LCC_pv_df)
         simpledt_LCC_pv_dt = simpledt_LCC_pv_df.datatable
         self.table_LCC_pv = simpledt_LCC_pv_dt
 
-        simpledt_SPC_df = DataFrame(SPC_res_df)
-        simpledt_SPC_dt = simpledt_SPC_df.datatable
-        self.table_SPC = simpledt_SPC_dt
+        simpledt_SPC_income_df = DataFrame(SPC_res_income_df)
+        simpledt_SPC_income_dt = simpledt_SPC_income_df.datatable
+        self.table_SPC_income = simpledt_SPC_income_dt
+        simpledt_SPC_payments_df = DataFrame(SPC_res_payments_df)
+        simpledt_SPC_payments_dt = simpledt_SPC_payments_df.datatable
+        self.table_SPC_payments = simpledt_SPC_payments_dt
+
         simpledt_SPC_check_df = DataFrame(SPC_check_df)
         simpledt_SPC_check_dt = simpledt_SPC_check_df.datatable
         self.table_SPC_check = simpledt_SPC_check_dt
@@ -256,14 +285,18 @@ class Results(ft.Stack):
         lv_02 = ft.ListView(
             expand=True, spacing=10, padding=10, auto_scroll=True, horizontal=False
         )
-        lv_02.controls.append(ft.Text('PSCでの公共側収支'))
-        lv_02.controls.append(self.table_PSC)
+        lv_02.controls.append(ft.Text('PSCでの公共側収支（収入）'))
+        lv_02.controls.append(self.table_PSC_income)
+        lv_02.controls.append(ft.Text('PSCでの公共側収支（支出）'))
+        lv_02.controls.append(self.table_PSC_payments)
         lv_02.controls.append(ft.Divider())
         lv_02.controls.append(ft.Text('PSCでの公共側キャッシュ・フローとその現在価値'))
         lv_02.controls.append(self.table_PSC_pv)
         lv_02.controls.append(ft.Divider())
-        lv_02.controls.append(ft.Text('PFI-LCCでの公共側収支'))
-        lv_02.controls.append(self.table_LCC)
+        lv_02.controls.append(ft.Text('PFI-LCCでの公共側収支（収入）'))
+        lv_02.controls.append(self.table_LCC_income)
+        lv_02.controls.append(ft.Text('PFI-LCCでの公共側収支（支出）'))
+        lv_02.controls.append(self.table_LCC_payments)
         lv_02.controls.append(ft.Divider())
         lv_02.controls.append(ft.Text('PFI-LCCでの公共側キャッシュ・フローとその現在価値'))
         lv_02.controls.append(self.table_LCC_pv)
@@ -274,8 +307,10 @@ class Results(ft.Stack):
         lv_03 = ft.ListView(
             expand=True, spacing=10, padding=10, auto_scroll=True, horizontal=False
         )
-        lv_03.controls.append(ft.Text('PFI-LCCでのSPC側収支'))
-        lv_03.controls.append(self.table_SPC)
+        lv_03.controls.append(ft.Text('PFI-LCCでのSPC側収支（収入）'))
+        lv_03.controls.append(self.table_SPC_income)
+        lv_03.controls.append(ft.Text('PFI-LCCでのSPC側収支（支出）'))
+        lv_03.controls.append(self.table_SPC_payments)
         lv_03.controls.append(ft.Divider())
         lv_03.controls.append(ft.Text('PFI-LCCでのSPCの返済資金確認結果'))
         lv_03.controls.append(self.table_SPC_check)
