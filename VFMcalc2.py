0dd4f8e6ed48019e83015773e368ea794d73e9d5
Yoshihiro Ashihara <yoshihiro.ashihara@gmail.com>
2025-01-25 20:03:20 +0900

    misc

--- VFMcalc2.py
+++ VFMcalc2.py
@@ -5,9 +5,26 @@ sys.dont_write_bytecode = True
 from tinydb import TinyDB, Query
 import save_results as sr
 import PSC_calc
+import PSC_calc_DBO
+import PSC_calc_BOT
+import PSC_calc_BT
 import LCC_calc
+import LCC_calc_DBO
+import LCC_calc_BOT
+import LCC_calc_BT
 import SPC_calc
-import make_present_value, risk_adjustment, check_SPC_cashForPPayment
+import SPC_calc_DBO
+import SPC_calc_BOT
+import SPC_calc_BT
+import make_present_value
+import risk_adjustment
+import risk_adjustment_DBO
+import risk_adjustment_BOT
+import risk_adjustment_BT
+import check_SPC_cashForPPayment
+import check_SPC_cashForPPayment_DBO
+import check_SPC_cashForPPayment_BOT
+import check_SPC_cashForPPayment_BT
 
 
 def VFM_calc():
@@ -24,47 +41,35 @@ def VFM_calc():
         risk_adjustment.risk_adj()
         make_present_value.make_pv()
         check_SPC_cashForPPayment.check_cash()
-         #Save_results()
         sr.make_df_addID_saveDB()
-        #sr.make_summary_add_ids()
-        #sr.save_db()
-        #view_saved.View_saved()
     
     if proj_type == "DBO(SPCなし)":
 
-        PSC_calc.PSC_calc()
-        LCC_calc.LCC_calc()
-        SPC_calc.SPC_calc()
-        risk_adjustment.risk_adj()
+        PSC_calc_DBO.PSC_calc()
+        LCC_calc_DBO.LCC_calc()
+        SPC_calc_DBO.SPC_calc()
+        risk_adjustment_DBO.risk_adj()
         make_present_value.make_pv()
-        check_SPC_cashForPPayment.check_cash()
-         #Save_results()
+        check_SPC_cashForPPayment_DBO.check_cash()
         sr.make_df_addID_saveDB()
-        #sr.make_summary_add_ids()
-        #sr.save_db()
-        #view_saved.View_saved()
     
     if proj_type == "BOT/BOO":
 
-        PSC_calc.PSC_calc()
-        LCC_calc.LCC_calc()
-        SPC_calc.SPC_calc()
-        risk_adjustment.risk_adj()
+        PSC_calc_BOT.PSC_calc()
+        LCC_calc_BOT.LCC_calc()
+        SPC_calc_BOT.SPC_calc()
+        risk_adjustment_BOT.risk_adj()
         make_present_value.make_pv()
-        check_SPC_cashForPPayment.check_cash()
-         #Save_results()
+        check_SPC_cashForPPayment_BOT.check_cash()
         sr.make_df_addID_saveDB()
-        #sr.make_summary_add_ids()
-        #sr.save_db()
-        #view_saved.View_saved()
     
     elif proj_type == "BT/DB(いずれもSPCなし)":
-        PSC_calc.PSC_calc()
-        LCC_calc.LCC_calc()
-        SPC_calc.SPC_calc()        
-        risk_adjustment.risk_adj()
+        PSC_calc_BT.PSC_calc()
+        LCC_calc_BT.LCC_calc()
+        SPC_calc_BT.SPC_calc()        
+        risk_adjustment_BT.risk_adj()
         make_present_value.make_pv()
-        check_SPC_cashForPPayment.check_cash()
+        check_SPC_cashForPPayment_BT.check_cash()
         sr.make_df_addID_saveDB()
 
 
