c6414e44ab4ab9cc2e0780182137cfb26e18a2ed
Yoshihiro Ashihara <yoshihiro.ashihara@gmail.com>
2025-01-28 14:00:06 +0900

    misc

--- Final_Inputs.py
+++ Final_Inputs.py
@@ -183,8 +183,8 @@ class Final_Inputs(ft.Column):
                         ],
                 ),
             ],
-            data_row_max_height=30.0,
-            heading_row_height=30,
+            data_row_max_height=50,
+            heading_row_height=50,
         )
         self.dt2 = ft.DataTable(
             columns=[
