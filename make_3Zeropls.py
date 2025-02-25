6c55cfd7130500cfd9372f3956b06adce947b4ec
Yoshihiro Ashihara <yoshihiro.ashihara@gmail.com>
2025-02-25 15:27:19 +0900

    misc

--- make_3Zeropls.py
+++
@@ -1,6 +0,0 @@
-import make_3pls_withZero
-
-zero_pl_PSC_income, zero_pl_PSC_payments, zero_pl_LCC_income, zero_pl_LCC_payments, zero_pl_SPC_income, zero_pl_SPC_payments = make_3pls_withZero.output()
-
-
-    
