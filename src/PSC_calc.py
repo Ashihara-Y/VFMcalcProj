3435417cb06ca34e1745c7eedafcfefba62c9e8b
Yoshihiro Ashihara <yoshihiro.ashihara@gmail.com>
2026-05-28 16:16:33 +0900

    Update PSC_calc.py

--- src/PSC_calc.py
+++ src/PSC_calc.py
@@ -16,9 +16,10 @@ engine = create_engine('sqlite:///VFM.db', echo=False, poolclass=NullPool, conne
 
 zero_pl_PSC_income, zero_pl_PSC_payments, zero_pl_LCC_income, zero_pl_LCC_payments, zero_pl_SPC_income, zero_pl_SPC_payments = make_3pls_withZero.output()
 
-def PSC_calc():
-    inputs_pdt = make_inputs_df.main()
-
+def PSC_calc(inputs=None):
+    #inputs_pdt = make_inputs_df.main()
+    inputs_pdt = inputs
+    
     PSC_shuushi_income = zero_pl_PSC_income
     PSC_shuushi_payments = zero_pl_PSC_payments
 
