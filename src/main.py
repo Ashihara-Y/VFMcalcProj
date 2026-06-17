42f2e60880bb7e40899a78932df99a7873caa4aa
Yoshihiro ASHIHARA <yoshihiro.ashihara@gmail.com>
2026-06-17 10:49:11 +0900

    misc

--- src/main.py
+++ src/main.py
@@ -230,5 +230,23 @@ async def main(page: ft.Page):
     #await page.push_route(page.route)
     route_change()
 
+from fastapi.responses import FileResponse
+import flet.fastapi as flet_fastapi
+from fastapi import FastAPI
+import pandas as pd
+import os
+
+app = FastAPI()
+    #app = flet_fastapi()
+
+@app.get("/api/download_local/{filename}")
+async def download_local_file(filename: str):
+    file_path = os.path.join(os.getcwd(), 'excels', filename)
+    if os.path.exists(filename):
+        return FileResponse(path=file_path, filename=filename, media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    else:
+        return {"error": "File not found"}
+
+app.mount("/", flet_fastapi.FletApp(main))
 
 ft.run(main, view=ft.AppView.WEB_BROWSER, port=8550) 
